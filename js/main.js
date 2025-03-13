fc808d606866519a0938c6a13805c403a71ab884
Kateryna Moskalenko <emochkaom@gmail.com>
2025-03-13 18:22:28 +0200

    task random pictures

---
+++ js/main.js
@@ -0,0 +1,26 @@
+'use.sctrict';
+const pics = [
+  " url('/img/1.png') ",
+  " url('/img/1.png')",
+   " url('/img/2.png')",
+     "url('/img/3.png')",
+    "url('/img/4.png')",
+    "url('/img/5.png')",
+  " url('/img/6.png')",
+    " url('/img/7.png')",
+    "url('/img/8.png')",
+   " url('/img/9.png')",
+    "url('/img/10.png')",
+];
+
+const btn = document.querySelector('.containerBtn');
+const containerPic = document.querySelector('.containerPic')
+
+btn.addEventListener('click', () => {
+    console.log('yes its me button');
+    let randomNum = Math.floor(Math.random() * pics.length);
+    let pic = pics[randomNum];
+    console.log(pic);
+    containerPic.style.backgroundImage = pic;  
+});
+
